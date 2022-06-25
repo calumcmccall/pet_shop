35c696b6f4d10ae50e4499041871c8de4f42544d
Calum <calumcmccall@gmail.com>
2022-06-25 15:44:05 +0100

    remove_customer_cash

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -39,3 +39,6 @@ def add_pet_to_stock(pet_shop, new_pet):
 
 def get_customer_cash(customer):
     return customer["cash"]
+
+def remove_customer_cash(customer, cash_to_remove):
+    customer["cash"] -= cash_to_remove
